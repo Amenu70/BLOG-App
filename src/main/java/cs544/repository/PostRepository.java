31af62a74c9ed57792d910874b089e2d1d955f52
Amensisa W <98834478+Amenu70@users.noreply.github.com>
2023-06-17 21:27:05 -0500

    Delete src/main/java/cs544/repository directory
    deleted repo

--- src/main/java/cs544/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package cs544.Repository;
-
-import cs544.Model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
